bbdcc6ddc256b82fd6e3cbbf49ea5156f6f9b655
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-09-29 11:09:33 +0200

    testing

---
+++ index.js
@@ -0,0 +1,25 @@
+let wrapper = document.getElementById("wrapper");
+
+let testimg = document.querySelector("img");
+
+testimg.addEventListener("click", clockTest);
+function clockTest (){
+    // testimg.classList.add("preAnimation");
+    setTimeout(() => {
+        // wrapper.classList.add("red");
+        testimg.classList.remove("preAnimation");
+        testimg.classList.add("animation");
+    }, 5000)
+}
+
+function theMath (size){
+    let time = size 
+
+    // const numerator = T_egg - T_water;
+    // const denominator = T - T_water;
+    
+    // const logValue = Math.log(numerator / denominator);
+    // const t = (m ** (2/3)) * K * logValue;
+    
+    // return t;
+}
