dfeff4009b3aabac8aec7540041e6b797d0f4b94
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-10-05 10:38:38 +0200

    tgrfedw

--- js/startUp.js
+++ js/startUp.js
@@ -4,15 +4,17 @@ export const main = document.querySelector("main");
 
 export function startUp() {
    document.getElementById("cssSwitch").setAttribute("href", "css/startUp.css");
- main.setAttribute("id", "startUpWrapper");
+   main.setAttribute("id", "startUpWrapper");
 
     main.innerHTML = `
     <div class="contentStartUp"> 
      </div>`
 
     setTimeout(() => {
-       // wrapper.innerHTML =``;
-       renderMainPage();
+      let whiteScreen = document.getElementById("whiteScreen");
+      whiteScreen.style.display = "block";
+      setTimeout(() => {whiteScreen.style.opacity = "1";}, 100);
+      setTimeout(renderMainPage, 400);
     }, 4000)   
 
 }
